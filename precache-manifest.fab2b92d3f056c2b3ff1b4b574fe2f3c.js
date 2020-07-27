823654befa84e058fd8b2777e6fa9270d3f1c880
Aadhar Jain <aadharjain1@gmail.com>
2020-07-27 11:59:42 +0530

    Updates

---
+++ precache-manifest.fab2b92d3f056c2b3ff1b4b574fe2f3c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124c6ca7eb21ed86b2de603243976903",
+    "url": "/deck-of-cards/index.html"
+  },
+  {
+    "revision": "522f1f655d4566f17f47",
+    "url": "/deck-of-cards/static/css/main.479c0891.chunk.css"
+  },
+  {
+    "revision": "cec2491ec9616c8be042",
+    "url": "/deck-of-cards/static/js/2.7b7fdce2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deck-of-cards/static/js/2.7b7fdce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "522f1f655d4566f17f47",
+    "url": "/deck-of-cards/static/js/main.6b800139.chunk.js"
+  },
+  {
+    "revision": "a3b418993c892e5846e3",
+    "url": "/deck-of-cards/static/js/runtime-main.70b577be.js"
+  },
+  {
+    "revision": "32c2f0d6790f44b78d8bc08b65c24111",
+    "url": "/deck-of-cards/static/media/clubs.32c2f0d6.png"
+  },
+  {
+    "revision": "66786da38249af4e85ae510204a28c14",
+    "url": "/deck-of-cards/static/media/diamonds.66786da3.png"
+  },
+  {
+    "revision": "236e820da2c4c8b53543664cdbafabc5",
+    "url": "/deck-of-cards/static/media/hearts.236e820d.png"
+  },
+  {
+    "revision": "12977290b6aef94579cfc52dc3a2bb69",
+    "url": "/deck-of-cards/static/media/spades.12977290.png"
+  }
+]);
